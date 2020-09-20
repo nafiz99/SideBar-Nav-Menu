3827a1027b5d00f054075c792b841b7dec1956bf
Nafiz Imtiaz <56600286+nafiz99@users.noreply.github.com>
2020-09-20 10:06:04 +0600

    Added closing icon

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,15 +1,29 @@
-import React from 'react'
+import React,{useState} from 'react'
 import {IoIosMenu} from "react-icons/io";
+import {AiOutlineCloseCircle} from "react-icons/ai";
 import {Link} from 'react-router-dom';
 
 
 function Navbar() {
+    const[sidebar,setSideBar]=useState(false)
+    const showSideBar=()=>setSideBar(!sidebar)
+    
     return (
        <>
        <div className="navbar">
            <Link to="#" className='menu-bars'></Link>
-           <IoIosMenu/>
+           <IoIosMenu onClick={showSideBar}/>
        </div>
+       <nav className={sidebar?'nav-menu-active':'nav-menu'}>
+           <ul className='nav-menu-items'>
+               <li className='nav-bar-toggle'>
+                   <Link to='#' className='menu-bars'>
+                       <AiOutlineCloseCircle/>
+
+                   </Link>
+               </li>
+           </ul>
+       </nav>
        </>
     )
 }
