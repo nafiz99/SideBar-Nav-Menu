30c79851dd0c519c35e18b82dc1c9daed4440905
Nafiz Imtiaz <56600286+nafiz99@users.noreply.github.com>
2020-09-20 12:16:56 +0600

    Final

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch,Route } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Reports from './pages/Reports';
+import Products from './pages/Products';
 
 function App() {
   return (
     <>
       <Router>
-      <Navbar/>
+        <Navbar />
         <Switch>
-          <Route path='/' />
+          <Route path='/' exact component={Home} />
+          <Route path='/reports' component={Reports} />
+          <Route path='/products' component={Products} />
         </Switch>
       </Router>
     </>
